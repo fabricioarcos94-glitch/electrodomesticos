8d017b03859915a95c03abfd916b29a0136cc71e
fabricioarcos94-glitch <fabricioarcos94@gmail.com>
2026-08-13 00:40:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// La lógica del catálogo está incluida en index.html.
+// Este archivo queda separado para que puedas añadir funciones futuras:
+// carrito, favoritos, precios, stock, login de administrador, etc.
